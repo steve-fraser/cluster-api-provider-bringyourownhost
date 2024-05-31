efbd91fc25e0eb20647bf7bbd758a3ed34017d16
Steve Fraser <steve@cast.ai>
2024-05-31 08:56:11 -0400

    fix tag for repo

--- installer/bundle_builder/ingredients/deb/download.sh
+++ installer/bundle_builder/ingredients/deb/download.sh
@@ -24,6 +24,6 @@ echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.
 
 echo Update apt package index, install kubelet, kubeadm and kubectl
 sudo apt-get update
-sudo apt-get download {kubelet,kubeadm,kubectl}:$ARCH=$KUBERNETES_VERSION
+sudo apt-get download {kubelet,kubeadm,kubectl}:$ARCH=$KUBERNETES_VERSION-1.1
 sudo apt-get download kubernetes-cni:$ARCH=1.2.0-2.1
 sudo apt-get download cri-tools:$ARCH=1.27.1-1.1
