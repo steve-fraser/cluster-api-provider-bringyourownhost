f0faddb5fea04b797a8ebc4f350bc3bdf844bbea
Steve Fraser <steve@cast.ai>
2024-06-03 14:58:39 -0400

    download any version of cri-tools

--- installer/bundle_builder/ingredients/deb/download.sh
+++ installer/bundle_builder/ingredients/deb/download.sh
@@ -26,4 +26,4 @@ echo Update apt package index, install kubelet, kubeadm and kubectl
 sudo apt-get update
 sudo apt-get download {kubelet,kubeadm,kubectl}:$ARCH=$KUBERNETES_VERSION-1.1
 sudo apt-get download kubernetes-cni:$ARCH=1.2.0-2.1
-sudo apt-get download cri-tools:$ARCH=$K8S_MAJOR.1-1.1
+sudo apt-get download cri-tools
